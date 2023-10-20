d592a53b2e5eca6f5dc4c2dc282d67fec375f2c0
Breno Santos <brenosantos145@gmail.com>
2023-10-20 11:33:44 -0300

    feat: creating chi router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,52 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/breno5g/chi-rest-api/configs"
+	"github.com/breno5g/chi-rest-api/internal/entity"
+	"github.com/breno5g/chi-rest-api/internal/infra/database"
+	"github.com/breno5g/chi-rest-api/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	config, _ := configs.InitConfig(".")
-	fmt.Println(config)
+	configs, err := configs.InitConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.WithValue("jwt", configs.TokenAuth))
+	r.Use(middleware.WithValue("JwtExperesIn", configs.JWTExpiresIn))
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+	r.Post("/users", userHandler.Create)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", configs.WebServerPort), r)
 }
